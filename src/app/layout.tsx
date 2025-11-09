9dffbf90709e0a7ec6be784a1243c3a2def5a745
Tristan <tjcoops1796@gmail.com>
2025-11-09 01:50:17 +0000

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import SidebarNav from "../components/navigation/SidebarNav";
 
@@ -41,6 +42,7 @@ export default function RootLayout({
           </section>
         </main>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
